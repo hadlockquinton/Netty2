2234403857f69ce4decb0383875a98de372f7c2f
hadlockquinton <hadlock.quinton@gmail.com>
2024-12-09 09:13:20 -0500

    Update and rename ImportantAhShit to ImportantAhShit.sh

---
+++ ImportantAhShit.sh
@@ -0,0 +1,17 @@
+Float
+
+    "Student#: student5"
+
+    Command: ssh student@10.50.32.232 -X
+
+    Password: password
+
+
+Q: What the hell is a float? 
+  A:
+Q: How many points to pass? Bare ass miniumum.
+    A: 1770 points
+Q: What OS is relavant?
+    A: Linux Kung FU! 
+Q:
+    A:
